68552da2bec07417f4e866c0ffeaadbdf026c840
Beth Spencer <beth.spencer@student.labette.edu>
2020-10-22 23:20:35 -0500

    added forecast header

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ var windEl = document.getElementById("wind-speed");
 var humidityEl = document.getElementById("humidity");
 var tempEl = document.getElementById("temperature");
 var uvEl = document.getElementById("UV-index");
-var pictureEl = document.getElementById("picture");
-
+var forecastEl = document.getElementById("forecast")
 var apiKey = "92139082328bddaf4605f68691c71b7b";
 
 function getWeather (cityName) {
@@ -18,13 +17,61 @@ function getWeather (cityName) {
         }).then(function(response_two){
             console.log(response_two);
             document.querySelector("#city-name").innerHTML = (response.name)
-            document.querySelector("#temperature").innerHTML = "Temperature: " + (+response_two.current.temp - 273.15) * 9/5 + 32 + "°F"
-            document.querySelector("#humidity").innerHTML = "Humidity: " + (+response_two.current.humidity)
-            document.querySelector("#wind-speed").innerHTML = "Wind Speed: " + (+response_two.current.wind_speed)
+            document.querySelector("#temperature").innerHTML = "Temperature: " + (+response_two.current.temp) + "°F"
+            document.querySelector("#humidity").innerHTML = "Humidity: " + (+response_two.current.humidity) + "%"
+            document.querySelector("#wind-speed").innerHTML = "Wind Speed: " + (+response_two.current.wind_speed) + "MPH"
             document.querySelector("#UV-index").innerHTML = "UV Index: " + (+response_two.current.uvi)
+
+            var temp =(response.main.temp - 273.15) * 1.80 + 32;
+        $(temperature).html((temp).toFixed(2)+"&#8457");
         })
+
     })
 }
+
+
+function getForecast (cityName) {
+    $.ajax({ 
+        url:"api.openweathermap.org/data/2.5/forecast?id="+cityName+"&appid="+apiKey,
+        method: "GET"})
+      .then(function(response_two){
+        console.log(response_two);
+        document.querySelector("#forecast").innerHTML = (response_two.daily[i])
+      })
+    
+}
+
+function returnWeatherForecast(cityName) {
+    let queryURL = "api.openweathermap.org/data/2.5/forecast?id="+cityName+"&appid="+apiKey;
+
+    $.get(queryURL).then(function(response){
+        let forecastInfo = response.list;
+        forecastDiv.empty();
+        $.each(forecastInfo, function(i) {
+            if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
+                return;
+            }
+            let forecastDate = new Date(forecastInfo[i].dt*1000);
+            let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
+
+            forecastDiv.append(`
+            <div class="col-md">
+                <div class="card text-white bg-primary">
+                    <div class="card-body">
+                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                        <img src=${weatherIcon} alt="Icon">
+                        <p>Temp: ${forecastInfo[i].main.temp} &#176;C</p>
+                        <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
+                    </div>
+                </div>
+            </div>
+            `)
+        })
+    })
+};
+
+
+
 
 document.querySelector("#searchButton").addEventListener("click",function(event){
     event.preventDefault();
@@ -32,3 +79,9 @@ document.querySelector("#searchButton").addEventListener("click",function(event)
     console.log(city)
     getWeather(city);
 })
+
+
+
+
+
+// http://api.weatherapi.com/v1/forecast.xml?key=92139082328bddaf4605f68691c71b7b&q=07112&days=5
